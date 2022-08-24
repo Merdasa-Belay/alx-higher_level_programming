5eb456341c4726659aabe41dbd012400a53d8e8f
robel <robelderbe@gmail.com>
2022-08-24 05:44:15 +0300

    [Fixed errors]

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,49 +1,26 @@
-#include "lists.h" 
-  
- /** 
-  * insert_node - inserts a new node 
-  * at a given position. 
-  * @head: head of a list. 
-  * @number: index of the list where the new node is 
-  * added. 
-  * Return: the address of the new node, or NULL if it 
-  * failed. 
-  */ 
- listint_t *insert_node(listint_t **head, int number) 
- { 
-         listint_t *new; 
-         listint_t *h; 
-         listint_t *h_prev; 
-  
-         h = *head; 
-         new = malloc(sizeof(listint_t)); 
-  
-         if (new == NULL) 
-                 return (NULL); 
-  
-         while (h != NULL) 
-         { 
-                 if (h->n > number) 
-                         break; 
-                 h_prev = h; 
-                 h = h->next; 
-         } 
-  
-         new->n = number; 
-  
-         if (*head == NULL) 
-         { 
-                 new->next = NULL; 
-                 *head = new; 
-         } 
-         else 
-         { 
-                 new->next = h; 
-                 if (h == *head) 
-                         *head = new; 
-                 else 
-                         h_prev->next = new; 
-         } 
-  
-         return (new); 
- }
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+  int n;
+  struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+listint_t *insert_node(listint_t **head, int number);
+
+#endif /* LISTS_H */
