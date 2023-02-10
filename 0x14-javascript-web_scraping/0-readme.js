4847560ea79ad590e3b1be368fe1514793bac43a
Merdasa-Belay <merdasabelay@gmail.com>
2023-02-10 03:39:12 +0300

    fixed error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const fs = require("fs");
-const file = process.argv[2];
-
-fs.readFile(file, "utf-8", function (err, data) {
-  if (err) {
-    console.log(err);
+const readme = require("fs");
+readme.readFile(process.argv[2], "utf-8", (error, response) => {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(data);
+    console.log(response);
   }
 });
